282c3d577772f6fda5cd7bb426f1135f7fcbf069
Aliaksandr.Sakalous <Aliaksandr.Sakalouski@seavus.com>
2018-04-16 23:19:34 +0300

    contunie

--- src/main/java/com/alsa/analysis/Capacity.java
+++ src/main/java/com/alsa/analysis/Capacity.java
@@ -11,7 +11,7 @@ public class Capacity {
     public static final ScoreOption[] options = ScoreOption.allOptions.toArray(new ScoreOption[0]);
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        /*ObjectOutputStream file = new ObjectOutputStream(new FileOutputStream("capacities.txt"));
+        ObjectOutputStream file = new ObjectOutputStream(new FileOutputStream("capacities.txt"));
         int [] capacities = new int [16777216];
         for (int i = 0; i < 16777216; ++i) {
             if (i % 1000000 == 0) {
@@ -22,15 +22,15 @@ public class Capacity {
         System.out.println("DONE!");
         file.writeObject(capacities);
         file.flush();
-        file.close();*/
+        file.close();
 
         /*ObjectInputStream file = new ObjectInputStream(new FileInputStream("capacities.txt"));
         int []  capacities = (int[]) file.readObject();
         for (int i = 15777216; i < 16777216; ++i) {
             System.out.println(capacities[i]);
         }*/
-        int capacity = capacity(16760311);
-        System.out.println(capacity);
+        //int capacity = capacity(16760311);
+        //System.out.println(capacity);
     }
 
     /**
@@ -49,6 +49,7 @@ public class Capacity {
                 if (maxscore < score1) {
                     maxscore = score1;
                 }
+                if (Integer.bitCount(deck1) < 3) continue;
                 for (int i2 = i1 + 1; i2 < options.length; ++i2) {
                     if ((deck1 & options[i2].option) == options[i2].option) {
                         int score2 = score1 + options[i2].score;
@@ -57,6 +58,7 @@ public class Capacity {
                         if (maxscore < score2) {
                             maxscore = score2;
                         }
+                        if (Integer.bitCount(deck2) < 3) continue;
                         for (int i3 = i2 + 1; i3 < options.length; ++i3) {
                             if ((deck2 & options[i3].option) == options[i3].option) {
                                 int score3 = score2 + options[i3].score;
@@ -65,6 +67,7 @@ public class Capacity {
                                 if (maxscore < score3) {
                                     maxscore = score3;
                                 }
+                                if (Integer.bitCount(deck3) < 3) continue;
                                 for (int i4 = i3 + 1; i4 < options.length; ++i4) {
                                     if ((deck3 & options[i4].option) == options[i4].option) {
                                         int score4 = score3 + options[i4].score;
@@ -73,6 +76,7 @@ public class Capacity {
                                         if (maxscore < score4) {
                                             maxscore = score4;
                                         }
+                                        if (Integer.bitCount(deck4) < 3) continue;
                                         for (int i5 = i4 + 1; i5 < options.length; ++i5) {
                                             if ((deck4 & options[i5].option) == options[i5].option) {
                                                 int score5 = score4 + options[i5].score;
@@ -81,6 +85,7 @@ public class Capacity {
                                                 if (maxscore < score5) {
                                                     maxscore = score5;
                                                 }
+                                                if (Integer.bitCount(deck5) < 3) continue;
                                                 for (int i6 = i5 + 1; i6 < options.length; ++i6) {
                                                     if ((deck5 & options[i6].option) == options[i6].option) {
                                                         int score6 = score5 + options[i6].score;
@@ -89,6 +94,7 @@ public class Capacity {
                                                         if (maxscore < score6) {
                                                             maxscore = score6;
                                                         }
+                                                        if (Integer.bitCount(deck6) < 3) continue;
                                                         for (int i7 = i6 + 1; i7 < options.length; ++i7) {
                                                             if ((deck6 & options[i7].option) == options[i7].option) {
                                                                 int score7 = score6 + options[i7].score;
@@ -97,6 +103,7 @@ public class Capacity {
                                                                 if (maxscore < score7) {
                                                                     maxscore = score7;
                                                                 }
+                                                                if (Integer.bitCount(deck7) < 3) continue;
                                                                 for (int i8 = i7 + 1; i8 < options.length; ++i8) {
                                                                     if ((deck7 & options[i8].option) == options[i8].option) {
                                                                         int score8 = score7 + options[i8].score;
